1cfde41903458939b678ea7d458ce9b8eda9354d
Stanislav Gurin <work.gurin@gmail.com>
2017-06-13 14:35:42 +0200

    When example

--- src/Main.kt
+++ src/Main.kt
@@ -2,18 +2,30 @@
  * Created by Stanislav Gurin on 13.06.2017.
  */
 
+import Color.*
+
 fun main(args: Array<String>) {
+    println(getColor(WHITE))
+    getColorWithSet(setOf(RED, GREEN))
+    likeManyIf(2, 4)
+}
 
-    val color: Color = Color.GREEN
-    val colorName: String = Color.RED.name
-    val colorHex: String = Color.RED.colorHex
-    val colorOpacity: Double? = Color.RED.opacity
+fun getColor(color: Color) = when (color) {
+    BLACK, WHITE -> "Black or White"
+    RED -> "Red"
+    GREEN -> "Green"
+    else -> "No color"
+}
 
-    println(color)
-    println(colorName)
-    println(colorHex)
-    println(colorOpacity)
+fun getColorWithSet(colorSet: Set<Color>) = when (colorSet) {
+    setOf(BLACK, WHITE) -> println("Black or White")
+    setOf(RED, GREEN) -> println("Red or Green")
+    else -> println("No color")
+}
 
-    Color.BLACK.some()
-
+fun likeManyIf(a: Int, b: Int) = when {
+    (a > b) -> println("$a > $b = true")
+    (a < b) -> println("$a < $b = true")
+    (a == b) -> println("$a == $b = true")
+    else -> println("Something other")
 }
